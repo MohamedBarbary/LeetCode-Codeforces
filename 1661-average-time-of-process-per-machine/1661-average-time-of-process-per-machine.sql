a878781941eac458643634c00f49bdacc7128c1e
Mohamed Nabil <99597455+MohamedBarbary@users.noreply.github.com>
2024-08-03 19:21:13 +0300

    Time: 282 ms (13.27%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -3,5 +3,5 @@ select a1.machine_id ,
   Round(AVG(a2.timestamp-a1.timestamp),3) AS processing_time
              from Activity a1,Activity a2
 where a1.machine_id = a2.machine_id  and a1.process_id =a2.process_id 
-             and a2.activity_type ='end' and a1.activity_type ='start'
+             and a2.timestamp>a1.timestamp      
                          group by a1.machine_id;
